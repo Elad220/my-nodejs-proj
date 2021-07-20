c921b3276bc8fa85d7243fb1c1aa870555be13ec
Elad Asaf <Elad220@gmail.com>
2021-07-20 16:12:55 +0300

    bla

--- webscrape.js
+++ webscrape.js
@@ -6,6 +6,22 @@ const host = 'localhost';
 const port = 8080;
 
 const app = http.createServer((req, resp) => {
+    
+    request("https://www.bbc.com", (error, response, html) => {
+        if(!error && response.statusCode==200) 
+        {
+            const $= cheerio.load(html);
+            const headline= $('h3');
+            const output = headline.first().text();
+            const article= $("p");
+            const articleText = article.first().text();
+            console.log(output);
+            console.log(articleText);
+            resp.writeHead(200, {"Content-Type": "text/plain"});
+            resp.writeContinue(output + '\n' + articleText);
+        }
+
+    });
     request("https://www.ynet.co.il/home/0,7340,L-8,00.html", (error, response, html) => {
         if(!error && response.statusCode==200) 
         {
@@ -22,22 +38,6 @@ const app = http.createServer((req, resp) => {
         }
 
     });
-    request("https://www.bbc.com", (error, response, html) => {
-        if(!error && response.statusCode==200) 
-        {
-            const $= cheerio.load(html);
-            const headline= $('h3');
-            const output = headline.first().text();
-            const article= $("p");
-            const articleText = article.first().text();
-            console.log(output);
-            console.log(articleText);
-            resp.writeHead(200, {"Content-Type": "text/plain"});
-            resp.write(output + '\n' + articleText);
-            resp.end();
-        }
-
-    });
 });
-console.log("Starting browser..")
+console.log("Starting server..")
 app.listen(port);
