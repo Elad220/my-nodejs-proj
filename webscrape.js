f8052f2e7b79e206c1a9eaf804df36f1f5c58f0a
Elad Asaf <Elad220@gmail.com>
2021-07-20 15:20:46 +0300

    update

--- webscrape.js
+++ webscrape.js
@@ -3,9 +3,9 @@ const cheerio= require("cheerio");
 const { head } = require("request-promise");
 const http = require("http");
 const host = 'localhost';
-const port = 8000;
+const port = 8080;
 
-const app = http.createServer((request, response) => {
+const app = http.createServer((req, resp) => {
     request("https://www.ynet.co.il/home/0,7340,L-8,00.html", (error, response, html) => {
         if(!error && response.statusCode==200) 
         {
@@ -16,9 +16,9 @@ const app = http.createServer((request, response) => {
             const articleText = article.children().first().text();
             console.log(output);
             console.log(articleText);
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(`&lt;h1&gt;${output} \n ${articleText}.&lt;/h1&gt;`);
-            response.end();
+            resp.writeHead(200, {"Content-Type": "text/html"});
+            resp.write(output + '\n' + articleText);
+            resp.end();
         }
 
     });
@@ -37,4 +37,4 @@ const app = http.createServer((request, response) => {
     });
 });
 console.log("Starting browser..")
-app.listen(3000);
+app.listen(port);
